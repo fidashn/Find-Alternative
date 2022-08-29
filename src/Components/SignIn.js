db853c80373b5dfa5934012c2eff16df5fb322c7
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-29 13:22:23 +0300

    created sign in & sign up components + installed react router

---
+++ src/Components/SignIn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div>
+    <form action="" className='signInForm'>
+    Email:
+    <input type="text" placeholder="Email.."/>
+    Password:
+    <input type="text" placeholder="Password.."/>
+    </form>
+    </div>
+  )
+}
+
+export default SignIn
---
+++ src/Components/SignUp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+    <form action="" className='signUpForm'>
+    Full Name:
+    <input type="text" placeholder="Full Name.."/>
+    Email:
+    <input type="text" placeholder="Email.."/>
+    Password:
+    <input type="text" placeholder="Password.."/>
+    </form>
+    </div>
+  )
+}
+
+export default SignUp
