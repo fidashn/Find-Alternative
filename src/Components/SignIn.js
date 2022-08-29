ec723b919eb49ad2340f319590535f4ea1bfad0b
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-30 02:09:32 +0300

    firebase connection needs further setup

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import { useState } from 'react';
+import {
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { auth } from '../firebase';
+
 
 function SignIn() {
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+  
+
+
   return (
+    <>
     <div>
-    <form action="" className='signInForm'>
+    <form action="" className='signInForm' >
     Email:
-    <input type="text" placeholder="Email.."/>
+    <input type="text" placeholder="Email.."  onChange={(event) => {
+            setLoginEmail(event.target.value);
+          }}/>
     Password:
-    <input type="text" placeholder="Password.."/>
-    <button type='submit'>Submit</button>
+    <input type="password" placeholder="Password.." onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}/>
+    <button type='submit' onClick={login}>login</button>
     </form>
+    {/* {user?.email} */}
+
+    <button onClick={logout}> Sign Out </button>
     </div>
+    </>
   )
 }
 
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,16 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  } from 'firebase/auth';
+import { auth } from '../firebase';
+
 
 function SignUp() {
+
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  
+
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div>
     <form action="" className='signUpForm'>
     Full Name:
     <input type="text" placeholder="Full Name.."/>
     Email:
-    <input type="text" placeholder="Email.."/>
+    <input type="text" placeholder="Email.."  onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}/>
     Password:
-    <input type="text" placeholder="Password.."/>
-    <button type='submit'>Submit</button>
+    <input type="password" placeholder="Password.." onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}/>
+    <button type='submit' onClick={register}>Submit</button>
     </form>
     </div>
   )
